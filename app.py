21e5761e993e217edbe910c8f004e79c6f11fdd2
Roisul <rroisulislam@gmail.com>
2022-08-02 00:05:23 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  1 20:48:40 2022
+
+@author: roisul
+"""
+
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # To render results on html gui
+    
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = round(prediction[0],2)
+    if int(output) == 1:
+        pred_text = "Applicant is approved for loan."
+    else:
+        pred_text = "Applicant is rejected for loan."
+    
+    return render_template('index.html', prediction_text=pred_text)
+
+@app.route('/results', methods=['POST'])
+def results():
+
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
